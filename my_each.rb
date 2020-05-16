4fb252d296cfa1b16c1808bbd6c177f4225ba017
Luis Leandro <luchrist777@gmail.com>
2020-05-16 17:58:58 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,10 +1,9 @@
-def my_each(arg)
-    i = 0
-    while i < self.length
-      yield(self[i])
-      i+=1
-    end 
-    self
-  end
-
+def my_each(array)
+  # code here
+  i=0 
+  while i < array.length 
+    yield(array[i])
+    i += 1 
+  end 
+  array 
 end
